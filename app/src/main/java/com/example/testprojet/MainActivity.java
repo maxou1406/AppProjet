9fdd5ccf8700a4d50890830664685afa727a1004
maxou1406 <79210573+maxou1406@users.noreply.github.com>
2021-02-17 16:15:12 +0100

    init

---
+++ app/src/main/java/com/example/testprojet/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.testprojet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_clique;
+    TextView tv_maintext;
+    int on=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeFields();
+        btn_clique.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                tv_maintext.setText(R.string.lucasmax);
+
+                switchOnOff();
+
+            }
+        });
+    }
+
+
+    void switchOnOff(){
+        if(on==1){
+            btn_clique.setText("on");
+            on=0;
+        }else {
+            btn_clique.setText("off");
+            on=1;
+        }
+    }
+
+    void initializeFields(){
+        btn_clique=findViewById(R.id.button1);
+        tv_maintext=findViewById(R.id.tv_1);
+    }
+}
